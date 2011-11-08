5242c0187624381e9986ef1902401da7afb9546a
Robin Gareus <rgareus@priroda.local>
2011-11-08 13:36:26 +0100

    cleaned up OSX build script.

--- buildmac.sh
+++ buildmac.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
+PRODUCT_NAME="samplecat"
 VERSION=0.1.1+rg
-DMGFILE=/tmp/samplecat_v$VERSION.dmg
-echo $DMGFILE
+
+GTKDIR=${GTKDIR:-"$HOME/gtk/inst"}
+GTKLOADER=gtk-2.0/2.10.0/loaders
+PANGOPATH=pango/1.6.0/modules
+
+DMGFILE=/tmp/${PRODUCT_NAME}_v$VERSION.dmg
+echo "building: $DMGFILE"
 
 #NOREBUILD=1
 #NOCLEAN=1
-GTKDIR="$HOME/gtk/inst"
+
+##############################################################################
+
+if test -d $GTKDIR; then
+	export PKG_CONFIG_PATH=$GTKDIR/lib/pkgconfig/:$PKG_CONFIG_PATH
+else
+	echo "no GTK installation-prefix was specified."
+	exit;
+fi
 
 if test -z "$NOREBUILD"; then
-  ### use local GTK installations if any
-  if test -d $GTKDIR; then
-    export PKG_CONFIG_PATH=$GTKDIR/lib/pkgconfig/:$PKG_CONFIG_PATH
-  fi
-  #CFLAGS="-arch i386 -arch x86_64 -arch ppc -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5" LDFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5" ./configure --disable-dependency-tracking
-  CFLAGS="-arch i386" ./configure
+  CFLAGS="-arch i386 -arch x86_64 -arch ppc" ./configure --disable-dependency-tracking
+	make clean
   make || exit
 fi
 
-file src/samplecat || exit
-
+file src/samplecat
+test -f src/samplecat || exit
 
 ##############################################################################
 follow_dependencies () {
@@ -79,14 +89,14 @@ update_executable() {
 
 echo "------------------------------------"
 
-export TARGET_BUILD_DIR="$(pwd)/build/"
-export PRODUCT_NAME="samplecat"
+export TARGET_BUILD_DIR="$(pwd)/build"
+export PRODUCT_NAME
 export INSTALLED=""
 export CONTENTS_PATH="$TARGET_BUILD_DIR/${PRODUCT_NAME}.app/Contents"
 export LIBS_PATH="${CONTENTS_PATH}/Frameworks"
 export TARGET_PATH="${CONTENTS_PATH}/MacOS"
 export TARGET="${TARGET_PATH}/${PRODUCT_NAME}"
-BGPIC=$(pwd)/dmgbg.png
+BGPIC=$(pwd)/icons/dmgbg.png
 TOPDIR=$(pwd)
 SRCDIR=${TOPDIR}/src
 
@@ -94,17 +104,15 @@ echo "BASEDIR: ${TARGET_BUILD_DIR}"
 echo "TARGET : ${TARGET}"
 echo "LIBDIR : ${LIBS_PATH}"
 
-rm -rf ${TARGET_BUILD_DIR}
+rm -rf "$TARGET_BUILD_DIR"
 mkdir -p "${CONTENTS_PATH}/Resources/share/"
-mkdir -p $LIBS_PATH
-mkdir -p $TARGET_PATH
-rm -f $LIBS_PATH/*.dylib
+mkdir -p "$LIBS_PATH"
+mkdir -p "$TARGET_PATH"
 
 echo "------------------------------------"
 echo "installing files"
 
-cp -R $HOME/data/mime "${CONTENTS_PATH}/Resources/share/"
-cp -v ${TOPDIR}/${PRODUCT_NAME}.icns ${CONTENTS_PATH}/Resources
+cp -v ${TOPDIR}/icons/${PRODUCT_NAME}.icns ${CONTENTS_PATH}/Resources
 
 cat > "${TARGET_BUILD_DIR}/${PRODUCT_NAME}.app/Contents/Info.plist" << EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -134,10 +142,10 @@ EOF
 # install binaries
 install -m 755 ${SRCDIR}/${PRODUCT_NAME} ${TARGET}
 update_executable
-file "$TARGET"
+#file "$TARGET"
 
 echo "------------------------------------"
-echo "installing dependent libs.."
+echo "installing dependent libs..."
 
 cd $LIBS_PATH && MORELIBS=`otool -arch all -L * | egrep '\/((opt|usr)\/local\/lib|gtk\/inst\/lib)' | awk '{print $1}'` && cd - > /dev/null
 test -n "${MORELIBS}" && MORELIBS="$MORELIBS "
@@ -152,46 +160,54 @@ while [ "X$MORELIBS" != "X" ]; do
 done
 
 echo "------------------------------------"
-echo "updating executable"
+echo "updating references in executable"
 update_executable
+echo "------------------------------------"
 otool -arch all -L "$TARGET"
 
 echo "------------------------------------"
 echo "installing GTK/Pango modules"
-# install GTK resources
-cp -R ${GTKDIR}/lib/pango ${LIBS_PATH}/
-cp -R ~/gtkicons/ ${CONTENTS_PATH}/Resources/share/icons/ # XXX
-#cp -R ${GTKDIR}/lib/gtk-2.0 ${LIBS_PATH}/
-#cp -R ${GTKDIR}/etc/fonts ${CONTENTS_PATH}/Resources/etc/
-
 
 mkdir -p "${CONTENTS_PATH}/Resources/etc/pango"
+mkdir -p  ${LIBS_PATH}/${PANGOPATH}
+
+mkdir -p ${CONTENTS_PATH}/Resources/etc/gtk-2.0/
+mkdir -p ${LIBS_PATH}/${GTKLOADER}
+
+cp -R ${GTKDIR}/lib/${PANGOPATH}/*.so ${LIBS_PATH}/${PANGOPATH}/
+cp -R ${GTKDIR}/lib/${GTKLOADER}/*.so ${LIBS_PATH}/${GTKLOADER}/
+
+if test -d "$HOME/data/gtk-shared/"; then
+  cp -R $HOME/data/gtk-shared/mime "${CONTENTS_PATH}/Resources/share/"
+  cp -R $HOME/data/gtk-shared/samplecat-icons ${CONTENTS_PATH}/Resources/share/icons
+else
+  cp -R $GTKDIR/share/mime "${CONTENTS_PATH}/Resources/share/"
+  cp -R $GTKDIR/share/icons "${CONTENTS_PATH}/Resources/share/"
+fi
+
+# generate resource config files
 cat > "${CONTENTS_PATH}/Resources/etc/pango/pango.rc" << EOF
 [Pango]
 ModuleFiles=../Resources/etc/pango/pango.modules
 EOF
 
-sed  's/\/Users\/rgareus\/gtk\/inst\/lib/..\/Frameworks/g' \
+ESCAPEDGTKDIR="`echo \"$GTKDIR\" | sed 's/\\//\\\\\//g'`"
+
+sed  's/'"$ESCAPEDGTKDIR"'\/lib/..\/Frameworks/g' \
   ${GTKDIR}/etc/pango/pango.modules > ${CONTENTS_PATH}/Resources/etc/pango/pango.modules
 
-rm ${LIBS_PATH}/pango/1.6.0/modules/*.la
-for n in ${LIBS_PATH}/pango/1.6.0/modules/*.so; do
+sed  's/'"$ESCAPEDGTKDIR"'\/lib/..\/Frameworks/g' \
+  ${GTKDIR}/etc/gtk-2.0/gdk-pixbuf.loaders > ${CONTENTS_PATH}/Resources/etc/gtk-2.0/gdk-pixbuf.loaders
+
+# change symbols links of plugins
+for n in ${LIBS_PATH}/${PANGOPATH}/*.so; do
 	for m in `otool -L $n | grep "gtk/inst" | awk '{print $1}'`; do
 		BN=`basename $m`;
 		install_name_tool -change "$m" "@executable_path/../Frameworks/$BN" "$n";
 	done
 done
 
-mkdir -p ${LIBS_PATH}/gtk-2.0/2.10.0/loaders
-mkdir -p ${CONTENTS_PATH}/Resources/etc/gtk-2.0/
-
-cp -R ${GTKDIR}/lib/gtk-2.0/2.10.0/loaders/*.so ${LIBS_PATH}/gtk-2.0/2.10.0/loaders/
-
-sed  's/\/Users\/rgareus\/gtk\/inst\/lib/..\/Frameworks/g' \
-  ${GTKDIR}/etc/gtk-2.0/gdk-pixbuf.loaders > ${CONTENTS_PATH}/Resources/etc/gtk-2.0/gdk-pixbuf.loaders
-
-rm ${LIBS_PATH}
-for n in ${LIBS_PATH}/gtk-2.0/2.10.0/loaders/*.so; do
+for n in ${LIBS_PATH}/${GTKLOADER}/*.so; do
 	for m in `otool -L $n | grep "gtk/inst" | awk '{print $1}'`; do
 		BN=`basename $m`;
 		install_name_tool -change "$m" "@executable_path/../Frameworks/$BN" "$n";
@@ -203,13 +219,11 @@ echo "------------------------------------"
 echo "generate startup-script"
 mv -vf ${TARGET} ${TARGET}-bin
 cat > "${TARGET}" << EOF
-#!/bin/sh
+#!/bin/bash
 CWD="\`/usr/bin/dirname \"\$0\"\`"
 export XDG_DATA_HOME="\$CWD/../Resources/share/"
 export PANGO_RC_FILE="\$CWD/../Resources/etc/pango/pango.rc"
 export GDK_PIXBUF_MODULE_FILE="\$CWD/../Resources/etc/gtk-2.0/gdk-pixbuf.loaders"
-#export GTK_PATH="\$CWD/../Frameworks/:\$CWD/../Resources:."
-#unset GTK_RC_FILES
 cd "\$CWD"
 exec "\$CWD/${PRODUCT_NAME}-bin"
 EOF
@@ -218,6 +232,7 @@ chmod +x "${TARGET}"
 
 ##############################################################################
 #roll a DMG
+echo "------------------------------------"
 echo "generating DMG.."
 TMPFILE=/tmp/sctmp.dmg
 MNTPATH=/tmp/mnt/
