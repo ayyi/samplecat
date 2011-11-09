ae730e8c25fe63f84fd03fb84e8fe95fc27b7fd0
Robin Gareus <robin@gareus.org>
2011-11-09 18:16:25 +0100

    move spectrogram to analysis folder.

--- src/audio_analysis/spectrogram/spectrogram.c
+++ src/audio_analysis/spectrogram/spectrogram.c
@@ -48,18 +48,15 @@
 
 #include "typedefs.h"
 #include "support.h"
-#include "sndfile_common.h"
-#include "sndfile_window.h"
 
+#include "audio_decoder/ad.h"
+#include "audio_analysis/spectrogram/sndfile_window.h"
 #include "main.h"
 
 extern unsigned debug;
 
 typedef void (*SpectrogramReady)(const char* filename, GdkPixbuf*, gpointer);
 typedef void (*SpectrogramReadyTarget)(const char* filename, GdkPixbuf*, gpointer, void* target);
-
-
-#define TIMER_STOP FALSE
 
 #define MIN_WIDTH 640
 #define MIN_HEIGHT 480
@@ -156,27 +153,21 @@ get_colour_map_value (float value, double spec_floor_db, unsigned char colour [3
 	return ;
 } /* get_colour_map_value */
 
-
 static void
-read_mono_audio (SNDFILE * file, sf_count_t filelen, double * data, int datalen, int indx, int total)
+read_mono_audio (void * file, int64_t filelen, double * data, int datalen, int indx, int total)
 {
-	sf_count_t start ;
-
+	int64_t start = (indx * filelen) / total - datalen / 2 ;
 	memset (data, 0, datalen * sizeof (data [0])) ;
 
-	start = (indx * filelen) / total - datalen / 2 ;
-
 	if (start >= 0)
-		sf_seek (file, start, SEEK_SET) ;
-	else
-	{	start = -start ;
-		sf_seek (file, 0, SEEK_SET) ;
-		data += start ;
-		datalen -= start ;
-		} ;
-
-	sfx_mix_mono_read_double (file, data, datalen) ;
-
+		ad_seek (file, start);
+	else {
+		start = -start;
+		ad_seek (file, 0);
+		data += start;
+		datalen -= start;
+	}
+	ad_read_mono(file, data, datalen);
 	return ;
 } /* read_mono_audio */
 
@@ -220,42 +211,6 @@ calc_magnitude (const double * freq, int freqlen, double * magnitude)
 	return max ;
 } /* calc_magnitude */
 
-#ifdef NEVER
-static void
-_render_spectrogram (cairo_surface_t * surface, double spec_floor_db, float mag2d [MAX_WIDTH][MAX_HEIGHT], double maxval, double left, double top, double width, double height)
-{
-	dbg(0, "...");
-	unsigned char colour [3] = { 0, 0, 0 } ;
-	double linear_spec_floor ;
-	int w, h;
-
-	int stride = cairo_image_surface_get_stride (surface);
-
-	unsigned char* data = cairo_image_surface_get_data (surface);
-	memset (data, 0, stride * cairo_image_surface_get_height (surface)) ;
-	dbg(0, "height=%i stride=%i", cairo_image_surface_get_height (surface), stride);
-
-	linear_spec_floor = pow (10.0, spec_floor_db / 20.0) ;
-
-	for (w = 0 ; w < width ; w ++) {
-		for (h = 0 ; h < height ; h++) {
-
-			mag2d [w][h] = mag2d [w][h] / maxval ;
-			mag2d [w][h] = (mag2d [w][h] < linear_spec_floor) ? spec_floor_db : 20.0 * log10 (mag2d [w][h]) ;
-
-			get_colour_map_value (mag2d [w][h], spec_floor_db, colour);
-
-			int y = height + top - 1 - h;
-			int x = (w + left) * 4;
-			data [y * stride + x + 0] = colour [2];
-			data [y * stride + x + 1] = colour [1];
-			data [y * stride + x + 2] = colour [0];
-			data [y * stride + x + 3] = 0;
-		};
-	}
-	cairo_surface_mark_dirty (surface);
-}
-#endif
 
 static void
 _render_spectrogram_to_pixbuf (GdkPixbuf* pixbuf, double spec_floor_db, float mag2d [MAX_WIDTH][MAX_HEIGHT], double maxval, double left, double top, double width, double height)
@@ -288,36 +243,6 @@ _render_spectrogram_to_pixbuf (GdkPixbuf* pixbuf, double spec_floor_db, float ma
 		};
 	}
 }
-
-#ifdef NEVER
-static void
-render_heat_map (cairo_surface_t * surface, double magfloor, const RECT *r)
-{
-	unsigned char colour [3], *data ;
-	int w, h, stride ;
-
-	stride = cairo_image_surface_get_stride (surface) ;
-	data = cairo_image_surface_get_data (surface) ;
-
-	for (h = 0 ; h < r->height ; h++)
-	{	get_colour_map_value (magfloor * (r->height - h) / (r->height + 1), magfloor, colour) ;
-
-		for (w = 0 ; w < r->width ; w ++)
-		{	int x, y ;
-
-			x = (w + r->left) * 4 ;
-			y = r->height + r->top - 1 - h ;
-
-			data [y * stride + x + 0] = colour [2] ;
-			data [y * stride + x + 1] = colour [1] ;
-			data [y * stride + x + 2] = colour [0] ;
-			data [y * stride + x + 3] = 0 ;
-			} ;
-		} ;
-
-	cairo_surface_mark_dirty (surface) ;
-}
-#endif
 
 static inline void
 x_line (cairo_t * cr, double x, double y, double len)
@@ -378,141 +303,6 @@ calculate_ticks (double max, double distance, TICKS * ticks)
 	return divisions + 1 ;
 } /* calculate_ticks */
 
-#ifdef NEVER
-static void
-str_print_value (char * text, int text_len, double value)
-{
-	if (fabs (value) < 1e-10)
-		snprintf (text, text_len, "0") ;
-	else if (fabs (value) >= 10.0)
-		snprintf (text, text_len, "%1.0f", value) ;
-	else if (fabs (value) >= 1.0)
-		snprintf (text, text_len, "%3.1f", value) ;
-	else
-		snprintf (text, text_len, "%4.2f", value) ;
-
-	return ;
-}
-
-
-static void
-render_spect_border (cairo_surface_t* surface, const char* filename, double left, double width, double seconds, double top, double height, double max_freq)
-{
-	char text [512];
-	cairo_text_extents_t extents;
-	cairo_matrix_t matrix;
-
-	TICKS ticks;
-	int k, tick_count;
-
-	cairo_t* cr = cairo_create (surface);
-
-	cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
-	cairo_set_line_width (cr, BORDER_LINE_WIDTH);
-
-	/* Print title. */
-	cairo_select_font_face (cr, font_family, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-	cairo_set_font_size (cr, 1.0 * TITLE_FONT_SIZE);
-
-	snprintf (text, sizeof (text), "Spectrogram: %s", filename);
-	cairo_text_extents (cr, text, &extents);
-	cairo_move_to (cr, left + 2, top - extents.height / 2);
-	cairo_show_text (cr, text);
-
-	/* Print labels. */
-	cairo_select_font_face (cr, font_family, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL) ;
-	cairo_set_font_size (cr, 1.0 * NORMAL_FONT_SIZE) ;
-
-	/* Border around actual spectrogram. */
-	cairo_rectangle (cr, left, top, width, height) ;
-
-	tick_count = calculate_ticks (seconds, width, &ticks) ;
-	for (k = 0 ; k < tick_count ; k++)
-	{	y_line (cr, left + ticks.distance [k], top + height, TICK_LEN) ;
-		if (k % 2 == 1)
-			continue ;
-		str_print_value (text, sizeof (text), ticks.value [k]) ;
-		cairo_text_extents (cr, text, &extents) ;
-		cairo_move_to (cr, left + ticks.distance [k] - extents.width / 2, top + height + 8 + extents.height) ;
-		cairo_show_text (cr, text) ;
-		} ;
-
-	tick_count = calculate_ticks (max_freq, height, &ticks) ;
-	for (k = 0 ; k < tick_count ; k++)
-	{	x_line (cr, left + width, top + height - ticks.distance [k], TICK_LEN) ;
-		if (k % 2 == 1)
-			continue ;
-		str_print_value (text, sizeof (text), ticks.value [k]) ;
-		cairo_text_extents (cr, text, &extents) ;
-		cairo_move_to (cr, left + width + 12, top + height - ticks.distance [k] + extents.height / 4.5) ;
-		cairo_show_text (cr, text) ;
-		} ;
-
-	cairo_set_font_size (cr, 1.0 * NORMAL_FONT_SIZE) ;
-
-	/* Label X axis. */
-	snprintf (text, sizeof (text), "Time (secs)") ;
-	cairo_text_extents (cr, text, &extents) ;
-	cairo_move_to (cr, left + (width - extents.width) / 2, cairo_image_surface_get_height (surface) - 8) ;
-	cairo_show_text (cr, text) ;
-
-	/* Label Y axis (rotated). */
-	snprintf (text, sizeof (text), "Frequency (Hz)") ;
-	cairo_text_extents (cr, text, &extents) ;
-
-	cairo_get_font_matrix (cr, &matrix) ;
-	cairo_matrix_rotate (&matrix, -0.5 * M_PI) ;
-	cairo_set_font_matrix (cr, &matrix) ;
-
-	cairo_move_to (cr, cairo_image_surface_get_width (surface) - 12, top + (height + extents.width) / 2) ;
-	cairo_show_text (cr, text) ;
-
-	cairo_destroy (cr) ;
-}
-
-
-static void
-render_heat_border (cairo_surface_t * surface, double magfloor, const RECT *r)
-{
-	const char *decibels = "dB" ;
-	char text [512] ;
-	cairo_t * cr ;
-	cairo_text_extents_t extents ;
-	TICKS ticks ;
-	int k, tick_count ;
-
-	cr = cairo_create (surface) ;
-
-	cairo_set_source_rgb (cr, 1.0, 1.0, 1.0) ;
-	cairo_set_line_width (cr, BORDER_LINE_WIDTH) ;
-
-	/* Border around actual spectrogram. */
-	cairo_rectangle (cr, r->left, r->top, r->width, r->height) ;
-	cairo_stroke (cr) ;
-
-	cairo_select_font_face (cr, font_family, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL) ;
-	cairo_set_font_size (cr, 1.0 * NORMAL_FONT_SIZE) ;
-
-	cairo_text_extents (cr, decibels, &extents) ;
-	cairo_move_to (cr, r->left + (r->width - extents.width) / 2, r->top - 5) ;
-	cairo_show_text (cr, decibels) ;
-
-	tick_count = calculate_ticks (fabs (magfloor), r->height, &ticks) ;
-	for (k = 0 ; k < tick_count ; k++)
-	{	x_line (cr, r->left + r->width, r->top + ticks.distance [k], TICK_LEN) ;
-		if (k % 2 == 1)
-			continue ;
-
-		str_print_value (text, sizeof (text), -1.0 * ticks.value [k]) ;
-		cairo_text_extents (cr, text, &extents) ;
-		cairo_move_to (cr, r->left + r->width + 2 * TICK_LEN, r->top + ticks.distance [k] + extents.height / 4.5) ;
-		cairo_show_text (cr, text) ;
-		} ;
-
-	cairo_destroy (cr) ;
-}
-#endif
-
 static void
 interp_spec (float * mag, int maglen, const double *spec, int speclen)
 {
@@ -537,89 +327,8 @@ interp_spec (float * mag, int maglen, const double *spec, int speclen)
 	return ;
 } /* interp_spec */
 
-#ifdef NEVER
 static void
-render_to_surface (const RENDER * render, SNDFILE *infile, int samplerate, sf_count_t filelen, cairo_surface_t * surface)
-{
-	static double time_domain [10 * MAX_HEIGHT];
-	static double freq_domain [10 * MAX_HEIGHT];
-	static double single_mag_spec [5 * MAX_HEIGHT];
-	static float mag_spec [MAX_WIDTH][MAX_HEIGHT];
-
-	fftw_plan plan;
-	double max_mag = 0.0;
-	int width, height, w, speclen;
-
-	if (render->border) {
-		width = lrint (cairo_image_surface_get_width (surface) - LEFT_BORDER - RIGHT_BORDER) ;
-		height = lrint (cairo_image_surface_get_height (surface) - TOP_BORDER - BOTTOM_BORDER) ;
-	}
-	else
-	{	width = render->width;
-		height = render->height;
-	}
-
-	/*
-	**	Choose a speclen value that is long enough to represent frequencies down
-	**	to 20Hz, and then increase it slightly so it is a multiple of 0x40 so that
-	**	FFTW calculations will be quicker.
-	*/
-	speclen = height * (samplerate / 20 / height + 1) ;
-	speclen += 0x40 - (speclen & 0x3f) ;
-
-	if (2 * speclen > G_N_ELEMENTS (time_domain)) {
-		printf ("%s : 2 * speclen > G_N_ELEMENTS (time_domain)\n", __func__);
-		exit (1);
-	};
-
-	plan = fftw_plan_r2r_1d (2 * speclen, time_domain, freq_domain, FFTW_R2HC, FFTW_MEASURE | FFTW_PRESERVE_INPUT) ;
-	if (plan == NULL) {
-		printf ("%s : line %d : create plan failed.\n", __FILE__, __LINE__);
-		exit (1) ;
-	};
-
-	for (w = 0 ; w < width ; w++) {
-		double single_max;
-
-		read_mono_audio (infile, filelen, time_domain, 2 * speclen, w, width) ;
-
-		apply_window (time_domain, 2 * speclen) ;
-
-		fftw_execute (plan) ;
-
-		single_max = calc_magnitude (freq_domain, 2 * speclen, single_mag_spec) ;
-		max_mag = MAX (max_mag, single_max) ;
-
-		interp_spec (mag_spec [w], height, single_mag_spec, speclen) ;
-	};
-
-	fftw_destroy_plan (plan);
-
-	if (render->border) {
-		dbg(0, "with border...");
-		RECT heat_rect;
-
-		heat_rect.left = 12 ;
-		heat_rect.top = TOP_BORDER + TOP_BORDER / 2 ;
-		heat_rect.width = 12 ;
-		heat_rect.height = height - TOP_BORDER / 2 ;
-
-		_render_spectrogram (surface, render->spec_floor_db, mag_spec, max_mag, LEFT_BORDER, TOP_BORDER, width, height) ;
-
-		render_heat_map (surface, render->spec_floor_db, &heat_rect) ;
-
-		render_spect_border (surface, render->filename, LEFT_BORDER, width, filelen / (1.0 * samplerate), TOP_BORDER, height, 0.5 * samplerate) ;
-		render_heat_border (surface, render->spec_floor_db, &heat_rect) ;
-	}
-	else
-		_render_spectrogram (surface, render->spec_floor_db, mag_spec, max_mag, 0, 0, width, height);
-
-	return;
-}
-#endif
-
-static void
-render_to_pixbuf (const RENDER * render, SNDFILE *infile, int samplerate, sf_count_t filelen, GdkPixbuf* pixbuf)
+render_to_pixbuf (const RENDER * render, void *infile, int samplerate, int64_t filelen, GdkPixbuf* pixbuf)
 {
 	static double time_domain [10 * MAX_HEIGHT];
 	static double freq_domain [10 * MAX_HEIGHT];
@@ -661,7 +370,7 @@ render_to_pixbuf (const RENDER * render, SNDFILE *infile, int samplerate, sf_cou
 	for (w = 0 ; w < width ; w++) {
 		double single_max;
 
-		read_mono_audio (infile, filelen, time_domain, 2 * speclen, w, width) ;
+		read_mono_audio(infile, filelen, time_domain, 2 * speclen, w, width) ;
 
 		apply_window (time_domain, 2 * speclen) ;
 
@@ -681,62 +390,16 @@ render_to_pixbuf (const RENDER * render, SNDFILE *infile, int samplerate, sf_cou
 }
 
 
-#ifdef NEVER
-static void
-render_cairo_surface (const RENDER * render, SNDFILE *infile, int samplerate, sf_count_t filelen, cairo_surface_t* Xsurface, guchar** out)
-{
-	dbg(0, "...");
-
-	/*
-	**	CAIRO_FORMAT_RGB24 	 each pixel is a 32-bit quantity, with the upper 8 bits
-	**	unused. Red, Green, and Blue are stored in the remaining 24 bits in that order.
-	*/
-
-	dbg(0, "creating 32 bit image surface...");
-	cairo_surface_t* surface = cairo_image_surface_create (CAIRO_FORMAT_RGB24, render->width, render->height);
-	if (surface == NULL || cairo_surface_status (surface) != CAIRO_STATUS_SUCCESS) {
-		cairo_status_t status = cairo_surface_status (surface);
-		printf ("Error while creating surface : %s\n", cairo_status_to_string (status));
-		return;
-	};
-
-	cairo_surface_flush (surface);
-
-	render_to_surface (render, infile, samplerate, filelen, surface);
-
-	dbg(0, "done");
-//#if 0
-	cairo_status_t status;
-	status = cairo_surface_write_to_png (surface, "output.png");
-	if (status != CAIRO_STATUS_SUCCESS)
-		printf ("Error while creating PNG file : %s\n", cairo_status_to_string (status));
-
-//	cairo_surface_destroy (surface);
-//#else
-	//TODO we cannot destroy the surface until we have finished with this buffer.
-	*out = cairo_image_surface_get_data(surface);
-//#endif
-
-	return;
-}
-#endif
-
-
 static GdkPixbuf*
-render_pixbuf (const RENDER * render, SNDFILE *infile, int samplerate, sf_count_t filelen)
+render_pixbuf (const RENDER * render, void *infile, int samplerate, int64_t filelen)
 {
 	PF;
-
 	/*
 	**	CAIRO_FORMAT_RGB24 	 each pixel is a 32-bit quantity, with the upper 8 bits
 	**	unused. Red, Green, and Blue are stored in the remaining 24 bits in that order.
 	*/
-
-	#define NO_ALPHA FALSE
-	GdkPixbuf* pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, NO_ALPHA, 8, render->width, render->height);
-
+	GdkPixbuf* pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, /*alpha*/ FALSE, 8, render->width, render->height);
 	render_to_pixbuf (render, infile, samplerate, filelen, pixbuf);
-
 	return pixbuf;
 }
 
@@ -744,29 +407,15 @@ render_pixbuf (const RENDER * render, SNDFILE *infile, int samplerate, sf_count_
 static GdkPixbuf*
 render_sndfile (const RENDER* render)
 {
-	SF_INFO info;
+	struct adinfo nfo;;
 
-	if (render->log_freq) {
-		printf ("Error : --log-freq option not working yet.\n\n");
+	void* infile = ad_open (render->sndfilepath, &nfo);
+	if (!infile) {
 		return NULL;
 	};
 
-	memset(&info, 0, sizeof (info));
-
-	SNDFILE* infile = sf_open (render->sndfilepath, SFM_READ, &info);
-	if (infile == NULL) {
-		if(debug) printf ("Error : failed to open file '%s' : \n%s\n", render->sndfilepath, sf_strerror (NULL));
-		return NULL;
-	};
-
-#if 0
-	render_cairo_surface (render, infile, info.samplerate, info.frames, surface, out);
-#else
-	GdkPixbuf* pixbuf = render_pixbuf (render, infile, info.samplerate, info.frames);
-#endif
-
-	sf_close(infile);
-
+	GdkPixbuf* pixbuf = render_pixbuf(render, infile, nfo.sample_rate, nfo.frames);
+	ad_close(infile);
 	return pixbuf;
 }
 
@@ -780,33 +429,10 @@ check_int_range (const char * name, int value, int lower, int upper)
 		} ;
 } /* check_int_range */
 
-#if 0
-static void
-usage_exit (const char * argv0, int error)
-{
-	const char * progname ;
 
-	progname = strrchr (argv0, '/') ;
-	progname = (progname == NULL) ? argv0 : progname + 1 ;
-
-	printf ("\nUsage :\n\n    %s [options] <sound file> <img width> <img height> <png name>\n\n", progname) ;
-
-	puts (
-		"    Create a spectrogram as a PNG file from a given sound file. The\n"
-		"    spectrogram image will be of the given width and height.\n"
-		) ;
-
-	puts (
-		"    Options:\n"
-		"        --dyn-range=<number>   : Dynamic range (ie 100 for 100dB range)\n"
-		"        --no-border            : Drop the border, scales, heat map and title\n" 
-		/*-"        --log-freq             : Use a logarithmic frquency scale\n" -*/
-		) ;
-
-	exit (error) ;
-}
-#endif
-
+/*************************
+ * SoundCat render queue
+ */
 
 RENDER*
 render_new()
@@ -942,7 +568,6 @@ fft_thread(gpointer data)
 	return NULL;
 }
 
-
 static void
 send_message(Message* msg)
 {
@@ -959,7 +584,6 @@ send_message(Message* msg)
 
 	g_async_queue_push(msg_queue, msg);
 }
-
 
 void
 render_spectrogram(const char* path, SpectrogramReady callback, gpointer user_data)
@@ -994,7 +618,6 @@ render_spectrogram(const char* path, SpectrogramReady callback, gpointer user_da
 	dbg(1, "%s", render->filename);
 	send_message(m);
 }
-
 
 void
 cancel_spectrogram(const char* path)
@@ -1121,6 +744,7 @@ get_spectrogram(const char* path, SpectrogramReady callback, gpointer user_data)
 	g_free(cache_path);
 }
 
+/* SampleCat API */
 
 void
 get_spectrogram_with_target(const char* path, SpectrogramReady callback, void* target, gpointer user_data)
@@ -1146,5 +770,3 @@ get_spectrogram_with_target(const char* path, SpectrogramReady callback, void* t
 
 	get_spectrogram(path, ready, closure);
 }
-
-
--- src/audio_analysis/spectrogram/spectrogram.h
+++ src/audio_analysis/spectrogram/spectrogram.h
@@ -1,6 +1,8 @@
-
+#ifndef __SPECTROGRAM_H_
+#define __SPECTROGRAM_H_
+#include <gtk/gtk.h>
 void get_spectrogram             (const char* path, gpointer callback, gpointer);
 void get_spectrogram_with_target (const char* path, gpointer callback, void* target, gpointer);
 void render_spectrogram          (const char* path, gpointer callback, gpointer);
 void cancel_spectrogram          (const char* path);
-
+#endif
