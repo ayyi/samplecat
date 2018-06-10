fee377c4ee7cdde81bec9f01a17f565f2285bf9c
Ayyi <tim@ayyi.org>
2018-06-10 18:56:51 +0100

    Ensure libwaveform is built with GTK support.

--- experimental/gl-app/views/files.impl.c
+++ experimental/gl-app/views/files.impl.c
@@ -1,7 +1,7 @@
 /**
 * +----------------------------------------------------------------------+
 * | This file is part of Samplecat. http://ayyi.github.io/samplecat/     |
-* | copyright (C) 2017-2017 Tim Orford <tim@orford.org>                  |
+* | copyright (C) 2017-2018 Tim Orford <tim@orford.org>                  |
 * +----------------------------------------------------------------------+
 * | This program is free software; you can redistribute it and/or modify |
 * | it under the terms of the GNU General Public License version 3       |
@@ -9,6 +9,7 @@
 * +----------------------------------------------------------------------+
 *
 */
+#include "config.h"
 #include <glib.h>
 #include <glib-object.h>
 #include <gtk/gtk.h>
