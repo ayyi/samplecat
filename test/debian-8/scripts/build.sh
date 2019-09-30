4f389c1359755964f37544601965861130831450
Ayyi <tim@ayyi.org>
2019-09-30 20:47:25 +0100

    Relaxed libavfilter dependency

--- test/debian-8/scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /root && mkdir samplecat && cd samplecat && \
-	git clone https://github.com/ayyi/samplecat.git && cd samplecat && \
-	git submodule init && git submodule update && git submodule foreach git pull origin master && \
-	./autogen.sh && ./configure && make
